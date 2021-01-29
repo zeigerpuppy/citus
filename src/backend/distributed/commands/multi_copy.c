d3f2128d9c5c8ee145eac80d1bebc99d929d44e6
Onder Kalaci <onderkalaci@gmail.com>
2021-01-29 15:27:46 +0100

    w

--- src/backend/distributed/commands/multi_copy.c
+++ src/backend/distributed/commands/multi_copy.c
@@ -3745,7 +3745,7 @@ CopyGetPlacementConnection(HTAB *connectionStateHash, ShardPlacement *placement,
 								&durationMicrosecs);
 
 			elog(INFO, "HandleRemoteTransactionConnectionError: "
-					"Connection is NULL:PQstatus(connection->pgConn) %d  connectionId: %ld Difference durationSeconds:%d durationMicrosecse:%d", PQstatus(connection->pgConn),connection->connectionId,durationSeconds, durationMicrosecs);
+					"Connection is NULL:PQstatus(connection->pgConn) %d  connectionId: %ld Difference durationSeconds:%d durationMicrosecse:%d connectionState: %d", PQstatus(connection->pgConn),connection->connectionId,durationSeconds, durationMicrosecs, connection->connectionState);
 
 			return NULL;
 		}
